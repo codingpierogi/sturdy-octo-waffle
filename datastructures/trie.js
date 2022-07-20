256903053753506069b438008059b00ac44a4b87
codingpierogi <jon.pesta@gmail.com>
2022-07-20 11:48:58 +0000

    Added initial Trie implementation.

--- datastructures/trie.js
+++ datastructures/trie.js
@@ -1,3 +1,20 @@
-class Trie {}
+// Time Complexity: Insert/Search O(key_length)
+// Space Complexity: O(alphabet_size * key_length * N)
+//                   where N is the number of keys in the Trie
+
+const ALPHABET_SIZE = 26;
+
+class TrieNode {
+  constructor() {
+    this.endOfWord = false;
+    this.children = Array(ALPHABET_SIZE).fill(null);
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+}
 
 module.exports = Trie;
--- datastructures/trie.test.js
+++ datastructures/trie.test.js
@@ -1,5 +1,14 @@
 const Trie = require("./trie");
 
-test("should be true", () => {
-  expect(1).toBe(1);
+test("Trie root TrieNode should not be null", () => {
+  const trie = new Trie();
+  expect(trie.root).not.toBe(null);
+});
+
+test("Trie root TrieNode should be initialized", () => {
+  const trie = new Trie();
+  expect(trie.root.endOfWord).toBe(false);
+  expect(trie.root.children).toEqual(
+    expect.arrayContaining(Array(26).fill(null))
+  );
 });
