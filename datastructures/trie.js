eb7347cca5470d5e70a14bfac4972f869f78c31e
codingpierogi <jon.pesta@gmail.com>
2022-07-20 23:08:46 +0000

    Implemented has and insert Trie functionality.

--- datastructures/trie.js
+++ datastructures/trie.js
@@ -5,15 +5,51 @@
 const ALPHABET_SIZE = 26;
 
 class TrieNode {
-  constructor() {
+  constructor(value = null) {
     this.endOfWord = false;
     this.children = Array(ALPHABET_SIZE).fill(null);
+    this.value = value;
   }
 }
 
 class Trie {
   constructor() {
     this.root = new TrieNode();
+  }
+
+  has(word) {
+    let currentNode = this.root;
+    const letters = this.#splitLetters(word);
+    for (const letter of letters) {
+      const key = this.#hashKey(letter);
+      if (!currentNode.children[key]) return false;
+      currentNode = currentNode.children[key];
+    }
+    return currentNode.endOfWord;
+  }
+
+  insert(word) {
+    let currentNode = this.root;
+    const letters = this.#splitLetters(word);
+    for (const letter of letters) {
+      const key = this.#hashKey(letter);
+      if (!currentNode.children[key])
+        currentNode.children[key] = new TrieNode(letter);
+      currentNode = currentNode.children[key];
+    }
+    currentNode.endOfWord = true;
+  }
+
+  insertAll(words) {
+    words.forEach((word) => this.insert(word));
+  }
+
+  #splitLetters(word) {
+    return word.toLowerCase().split("");
+  }
+
+  #hashKey(letter) {
+    return letter.charCodeAt() - "a".charCodeAt();
   }
 }
 
--- datastructures/trie.test.js
+++ datastructures/trie.test.js
@@ -12,3 +12,24 @@ test("Trie root TrieNode should be initialized", () => {
     expect.arrayContaining(Array(26).fill(null))
   );
 });
+
+test("Trie.has should return false when missing word", () => {
+  const trie = new Trie();
+  expect(trie.has("the")).toBe(false);
+});
+
+test("Trie.has should return true when word inserted", () => {
+  const trie = new Trie();
+  trie.insert("the");
+  expect(trie.has("the")).toBe(true);
+});
+
+test("Trie should correctly insert and search itself", () => {
+  const words = ["the", "a", "there", "answer", "any", "by", "bye", "their"];
+  const trie = new Trie();
+  trie.insertAll(words);
+  expect(trie.has("the")).toBe(true);
+  expect(trie.has("these")).toBe(false);
+  expect(trie.has("their")).toBe(true);
+  expect(trie.has("thaw")).toBe(false);
+});
